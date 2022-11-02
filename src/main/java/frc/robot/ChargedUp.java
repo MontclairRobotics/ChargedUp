5ec39898ccee77a225e0cd0d0a6b86f21da143d1
Abraham17-K <100158282+BlindGalaxy4817@users.noreply.github.com>
2022-11-02 15:59:53 -0400

    Added Controllers

--- src/main/java/frc/robot/ChargedUp.java
+++ src/main/java/frc/robot/ChargedUp.java
@@ -5,19 +5,24 @@
 package frc.robot;
 
 import org.team555.frc.command.commandrobot.RobotContainer;
+import org.team555.frc.controllers.GameController;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import static frc.robot.constants.Controllers.*;
 
-public class ChargedUp extends RobotContainer
+public class ChargedUp extends RobotContainer 
 {
+    public static final GameController driverController = GameController.from(DRIVER_CONTROLLER_TYPE,
+            DRIVER_CONTROLLER_PORT);
+    public static final GameController operatorController = GameController.from(OPERATOR_CONTROLLER_TYPE,
+            OPERATOR_CONTROLLER_PORT);
+
     @Override
-    public void initialize() 
-    {
-        
+    public void initialize() {
 
         // CODE HERE :D
     }
