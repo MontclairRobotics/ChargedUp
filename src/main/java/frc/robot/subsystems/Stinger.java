7b9e6bdca2c26fe482e4adced4124c53f745bdae
Dylan Rafael <dylanrafael05@gmail.com>
2023-01-17 11:19:47 -0500

    Uncomment stinger implementation

--- src/main/java/frc/robot/subsystems/Stinger.java
+++ src/main/java/frc/robot/subsystems/Stinger.java
@@ -1,24 +1,24 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import org.team555.frc.command.commandrobot.ManagerSubsystemBase;
+import org.team555.frc.command.commandrobot.ManagerSubsystemBase;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-// import edu.wpi.first.wpilibj.PneumaticHub;
-// import edu.wpi.first.wpilibj.Solenoid;
-// import frc.robot.constants.Constants.Robot;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.constants.Constants.Robot;
 
-// public class Stinger extends ManagerSubsystemBase{
-//     public final CANSparkMax stingerMotor = new CANSparkMax (Robot.STINGER_PORT, MotorType.kBrushless);
-//     public void startExtending(){
+public class Stinger extends ManagerSubsystemBase{
+    public final CANSparkMax stingerMotor = new CANSparkMax (Robot.STINGER_PORT, MotorType.kBrushless);
+    public void startExtending(){
         
-//     }
-//     public void startRetracting(){
+    }
+    public void startRetracting(){
 
-//     }
-//     public void stop(){
+    }
+    public void stop(){
 
-//     }
+    }
     
-// }
+}
