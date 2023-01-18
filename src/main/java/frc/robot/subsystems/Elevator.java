20bf165bf850f7248181a9b35b97dfb8cf13fec7
Dylan <dylanrafael05@gmail.com>
2023-01-18 15:13:53 -0500

    Added elevator commands
    Make thing go up. Make thing go down. Make thing do the hokey pokey and turn itself around.
    
    THAT'S WHAT IT'S ALL ABOUT

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,27 +3,66 @@ package frc.robot.subsystems;
 import org.team555.frc.command.commandrobot.ManagerSubsystemBase;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants.*;
+import frc.robot.structure.PIDMechanism;
 
-public class Elevator {
+public class Elevator extends ManagerSubsystemBase {
     private CANSparkMax motor = new CANSparkMax(Robot.ELEVATOR_MOTOR_PORT, MotorType.kBrushless);
+
+    PIDMechanism elevatorPID = new PIDMechanism(Robot.elevatorUpDown());
+    RelativeEncoder elevatorEncoder = motor.getEncoder();
     
     public Elevator(){
         motor.setInverted(Robot.ELEVATOR_INVERTED);
+        elevatorEncoder.setPositionConversionFactor(Robot.ELEVATOR_UP_DOWN_CONVERSION_FACTOR);
     }
 
-    public void elevate(){
-        motor.set(Robot.ELEVATOR_SPEED);
+    /**
+     * Set elevator to HIGH position
+     * ELEVATOR_MAX_HEIGHT is defined in constants.java
+     */
+    public void makeHigh(){
+        elevatorPID.setTarget(Robot.ELEVATOR_MAX_HEIGHT);
     }
 
-    public void delevate(){
-        motor.set(-Robot.ELEVATOR_SPEED);
+    /**
+     * Set elevator to LOW position
+     */
+    public void makeLow() {
+        elevatorPID.setTarget(0);
     }
 
+    /**
+     * Raise elevator height manually at ELEVATOR_SPEED (constants.java)
+     */
+    public void lower(){
+        elevatorPID.setSpeed(Robot.ELEVATOR_SPEED);
+    }
+
+    /**
+     * Lower elevator height manually at ELEVATOR_SPEED (constants.java)
+     */
+    public void raise(){
+        elevatorPID.setSpeed(-Robot.ELEVATOR_SPEED);
+    }
+
+    /** 
+     * Set elevator speed to zero
+     */
     public void stop(){
-        motor.set(0);
+        elevatorPID.setSpeed(0);
+    }
+
+
+    // run PID
+    @Override
+    public void always() {
+        elevatorPID.setMeasurement(elevatorEncoder.getPosition());
+        elevatorPID.update();
+        motor.set(elevatorPID.getSpeed());
     }
 }
