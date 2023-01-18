1a3d49435802ef186d1770294523371ff4f30f6d
gliggy <leocfitz@gmail.com>
2023-01-18 15:07:10 -0500

    add method to set all leds to a certain color 2

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -4,6 +4,7 @@ import org.team555.frc.command.commandrobot.ManagerBase;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.ChargedUp;
 import frc.robot.constants.Constants;
 
@@ -11,9 +12,18 @@ public class LED extends ManagerBase {
     private AddressableLED led = new AddressableLED(Constants.Robot.LED_PWM_PORT);
     private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(150);
 
+    public LED() {
+        led.setLength(ledBuffer.getLength());
+    }
+
    
     @Override
     public void always() {
         led.setData(ledBuffer);
+    }
+    public void setColor(Color color) {
+        for (int i = 0; i < ledBuffer.getLength(); i++) {
+            ledBuffer.setLED(i, color);
+        }
     }
 }
