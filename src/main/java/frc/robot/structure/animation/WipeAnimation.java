d4a4f9b3ebe3672d094dd26bb0446daf3c1fd80b
gliggy <leocfitz@gmail.com>
2023-01-25 14:18:12 -0500

    finished wipe animation

--- src/main/java/frc/robot/structure/animation/WipeAnimation.java
+++ src/main/java/frc/robot/structure/animation/WipeAnimation.java
@@ -3,33 +3,27 @@ package frc.robot.structure.animation;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 
-public class WipeAnimation extends Animation
+public class WipeAnimation extends Animation 
 {
     private Color oldColor;
     private Color newColor;
-    private int index;
-    
-    public WipeAnimation(double time, Color oldColor, Color newColor)
+    private int index = 0;
+
+    public WipeAnimation(double time, Color oldColor, Color newColor) 
     {
         super(time);
         this.oldColor = oldColor;
         this.newColor = newColor;
     }
 
-    public void start(AddressableLEDBuffer ledBuffer) 
-    {
-        fill(ledBuffer, oldColor);
-        index = 0;
-        run(ledBuffer);
-    }
-
     public void run(AddressableLEDBuffer ledBuffer) 
     {
-        for (int i = 0; i<index; i++) 
-        {
+        fill(ledBuffer, oldColor);
+        for (int i = 0; i < index; i++) {
             ledBuffer.setLED(i, newColor);
+
         }
-        
         index += 3;
+
     }
 }
