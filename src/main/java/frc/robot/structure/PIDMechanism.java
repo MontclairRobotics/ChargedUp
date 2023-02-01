bc06c7a2c09738a50424d3e971d0d0d2feefb7b0
Caitie Farrel <caitiemfarrell@gmail.com>
2023-02-01 18:53:18 -0500

    Added Comments

--- src/main/java/frc/robot/structure/PIDMechanism.java
+++ src/main/java/frc/robot/structure/PIDMechanism.java
@@ -2,29 +2,6 @@ package frc.robot.structure;
 
 import edu.wpi.first.math.controller.PIDController;
 
-/*
-TODO: 1/17
-class PIDMechanism 
-{
-    PIDMechanism(PIDController) - constructor
-    PIDController controller()  - get the controller
-
-    void target(double) - target measurement
-    void cancel()       - cancel pidding
-
-    void setMeasurement(double) - set measurement for pid
-    void setSpeed(double)       - set speed directly. ignore if pidding
-
-    void update() - update speed
-
-    double get()     - get resultant speed
-    boolean active() - is pidding
-    
-    TODO: 1/18
-    void setTimeout(double) - set the maximum time which pidding will occur
-}
-
-*/
 
 public class PIDMechanism
 {
@@ -38,47 +15,86 @@ public class PIDMechanism
         this.pidController = pidController;
     }
 
+    /**
+     * Get the PIDController for this mechanism
+     * 
+     * @return PIDController for this mechanism
+     */
     public PIDController getPIDController()
     {
         return pidController;
     }
 
+    /**
+     * Set the target value (setPoint) that the mechanism moves to
+     * 
+     * @param target
+     */
     public void setTarget(double target)
     {
         pidController.setSetpoint(target);
         usingPID = true;
     }
 
+    /**
+     * Stop using PID to determine speed
+     */
     public void cancel()
     {
         usingPID = false;
     }
 
+    /**
+     * Set the measurement of where it currently is
+     * @param measurement
+     */
     public void setMeasurement(double measurement)
     {
         this.measurement = measurement;
     }
-
-    public void setSpeed(double speed) { //duck you fylan
+    /**
+     * Set the speed manually (without PID)
+     * @param speed
+     */
+    public void setSpeed(double speed) //duck you fylan
+    {
         this.speed = speed;
     }
-
+    /**
+     * Updates the calculated speed
+     * <p>
+     * <ul>
+     * <li>if we are currently using PID, calculate using PID
+     * <li>otherwise, use {@link PIDMechanism#setSpeed(double) setSpeed()} to set the speed manually
+     * </ul>
+     */
     public void update()
     {
+        //value = boolean ? if true set to this : else set to this
         speed = usingPID ? pidController.calculate(measurement) : speed;
 
         if(pidController.atSetpoint())
         {
             cancel();
         }
-        //value = boolean ? if true set to this : else set to this
     }
-
+    /**
+     * get the speed determined by the {@link PIDMechanism#update() update method}
+     * @return speed
+     */
     public double getSpeed() 
     {
         return speed;
     }
 
+    /**
+     * Get whether or not PID is used to calculate
+     * <ul>
+     *  <li> <code>true</code> if using PID
+     *  <li> <code>false</code> if <b>not</b> using PID
+     * </ul>
+     * @return boolean
+     */
     public boolean active()
     {
         return usingPID;
--- src/main/java/frc/robot/structure/factories/HashMaps.java
+++ src/main/java/frc/robot/structure/factories/HashMaps.java
@@ -5,10 +5,21 @@ import java.util.Map;
 
 public class HashMaps 
 {
+    /**
+     * Creates an empty HashMap
+     * 
+     * @return Empty HashMap
+     */
     public static <E,V> HashMap<E,V> of()
     {
         return new HashMap<E, V>();
     }
+    /**
+     * Create a new HashMap with the following key and value pair
+     * @param e1 key
+     * @param v1 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1)
     {
         var m = new HashMap<E, V>();
@@ -17,6 +28,14 @@ public class HashMaps
 
         return m;
     }
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key 1
+     * @param v1 value 1
+     * @param e2 key 2
+     * @param v2 value 2
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2)
     {
         var m = new HashMap<E, V>();
@@ -26,6 +45,16 @@ public class HashMaps
 
         return m;
     }
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key 1
+     * @param v1 value 1
+     * @param e2 key 2
+     * @param v2 value 2
+     * @param e3 key 3
+     * @param v3 value 3
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3)
     {
         var m = new HashMap<E, V>();
@@ -37,6 +66,18 @@ public class HashMaps
         return m;
     }
     
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key
+     * @param v1 value
+     * @param e2 key
+     * @param v2 value
+     * @param e3 key
+     * @param v3 value
+     * @param e4 key
+     * @param v4 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3, E e4, V v4)
     {
         var m = new HashMap<E, V>();
@@ -48,6 +89,20 @@ public class HashMaps
 
         return m;
     }
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key
+     * @param v1 value
+     * @param e2 key
+     * @param v2 value
+     * @param e3 key
+     * @param v3 value
+     * @param e4 key
+     * @param v4 value
+     * @param e5 key
+     * @param v5 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3, E e4, V v4, E e5, V v5)
     {
         var m = new HashMap<E, V>();
@@ -60,7 +115,22 @@ public class HashMaps
 
         return m;
     }
-    
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key
+     * @param v1 value
+     * @param e2 key
+     * @param v2 value
+     * @param e3 key
+     * @param v3 value
+     * @param e4 kwy
+     * @param v4 value
+     * @param e5 key
+     * @param v5 value
+     * @param e6 key
+     * @param v6 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3, E e4, V v4, E e5, V v5, E e6, V v6)
     {
         var m = new HashMap<E, V>();
@@ -74,7 +144,24 @@ public class HashMaps
 
         return m;
     }
-    
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key
+     * @param v1 value
+     * @param e2 key
+     * @param v2 value
+     * @param e3 key
+     * @param v3 value
+     * @param e4 key
+     * @param v4 value
+     * @param e5 key
+     * @param v5 value
+     * @param e6 key
+     * @param v6 value
+     * @param e7 key
+     * @param v7 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3, E e4, V v4, E e5, V v5, E e6, V v6, E e7, V v7)
     {
         var m = new HashMap<E, V>();
@@ -89,6 +176,26 @@ public class HashMaps
 
         return m;
     }
+    /**
+     * Create a new HashMap with the following key and value pairs
+     * @param e1 key
+     * @param v1 value
+     * @param e2 key
+     * @param v2 value
+     * @param e3 key
+     * @param v3 value
+     * @param e4 key
+     * @param v4 value
+     * @param e5 key
+     * @param v5 value
+     * @param e6 key
+     * @param v6 value
+     * @param e7 key
+     * @param v7 value
+     * @param e8 key
+     * @param v8 value
+     * @return HashMap containing all the keys and their respective values
+     */
     public static <E,V> HashMap<E,V> of(E e1, V v1, E e2, V v2, E e3, V v3, E e4, V v4, E e5, V v5, E e6, V v6, E e7, V v7, E e8, V v8)
     {
         var m = new HashMap<E, V>();
@@ -106,6 +213,11 @@ public class HashMaps
     }
     
     @SafeVarargs
+    /**
+     * Construct a HashMap with the inputted entries
+     * @param entries Array of Entries
+     * @return HashMap containing all the entries
+     */
     public static <E,V> HashMap<E,V> ofEntries(Map.Entry<E,V>... entries)
     {
         var m = new HashMap<E, V>();
@@ -119,3 +231,5 @@ public class HashMaps
     }
     
 }
+
+
