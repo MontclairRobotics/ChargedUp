b801ca68705910fa84dcde625153afc85598b289
Dylan Rafael <dylanrafael05@gmail.com>
2023-02-25 15:23:54 -0500

    Small fix

--- src/main/java/frc/robot/components/managers/Auto.java
+++ src/main/java/frc/robot/components/managers/Auto.java
@@ -25,7 +25,7 @@ public class Auto extends ManagerBase
     public Auto()
     {
         subscriber = ChargedUp.getMainTab()
-            .add("Auto Command", "")
+            .add("Auto Command", "<ERROR>")
             .withPosition(0, 0)
             .withSize(2, 1)
             .withWidget(BuiltInWidgets.kTextView)
