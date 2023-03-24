1e01d1e48a606426fb2b8fa661700867f6ed2f99
Dylan Rafael <dylanrafael05@gmail.com>
2023-03-24 09:57:21 -0400

    Various path fixes

--- src/main/java/org/team555/constants/Constants.java
+++ src/main/java/org/team555/constants/Constants.java
@@ -22,7 +22,7 @@ public final class Constants
     public static class Auto 
     {
         public static final double MAX_VEL = 4;
-        public static final double MAX_ACC = 1.2;
+        public static final double MAX_ACC = 1.5;
 
         public static PathConstraints constraints()
         {
